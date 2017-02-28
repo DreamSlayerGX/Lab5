3f053ff3f88f125001b5635a6a2b69d64cd7acb5
antonkdahlin <anton.nicolaus@gmail.com>
2017-02-28 17:10:45 +0100

    added toString to Time
    toString returns the time in hh:mm format

--- Time.java
+++ Time.java
@@ -1,7 +1,17 @@
 package simulator;
 
 public class Time {
-	
+	private double openTime;
 	private double time;
+	public Time(double time){
+		this.time = time;
+		this.openTime = 8.0;
+	}
+	public String toString(){
+		int hour = (int)(Math.floor(time+openTime));
+		int minute = (int)(Math.round((time-Math.floor(time))*60));
+		
+		return(hour+":"minute);
+	}
 
 }
