b0e61aeb90ee99b6a3adfb3787c4efec99e396a2
antonkdahlin <anton.nicolaus@gmail.com>
2017-03-01 17:28:46 +0100

    First update i time
    kan nu översätta till sträng med toString, har en start tid

--- src/simulator/Time.java
+++ src/simulator/Time.java
@@ -1,7 +1,24 @@
+
 package simulator;
 
 public class Time {
-	
+	private final double OPENTIME = 8.0;
 	private double time;
+	public Time(double time){
+		this.time = time;
+		
+	}
+	public Time(Time time){
+		this.time = time.time;
+	}
+	public double getTime(){
+		return time;
+	}
+	public String toString(){
+		int hour = (int)(Math.floor(time+OPENTIME));
+		int minute = (int)(Math.round((time-Math.floor(time))*60));
+		
+		return(hour+":"+minute);
+	}
 
 }
