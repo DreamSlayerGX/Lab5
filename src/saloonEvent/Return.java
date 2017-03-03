36b859f6e8754e2d10e978b248d45812ef2f63aa
antonkdahlin <anton.nicolaus@gmail.com>
2017-03-03 14:25:48 +0100

    Första verision av Return med funktion
    kör en startQueueTime om den ställer sig i kön, vet inte om det är så vi ska göra sen. if satserna är inte optimerade då det sker duplicering av kod

--- src/saloonEvent/Return.java
+++ src/saloonEvent/Return.java
@@ -5,15 +5,33 @@ import simulator.Time;
 
 public class Return extends CustomerEvent{
 
-	public Return(Time time) {
+	public Return(Time time, Customer customer) {
 		super(time, false);
-		
+		super.customer = customer;
 		
 	}
 
-	
+
+	/**
+	 * If a chair is free, occupy a chair. If there is space in queue, place customer last in return queue. If queue is full of returning customers, take a walk(create new return) and come back
+	 */
 	public void execute(Store store) {
-		
+		//om ledig stol finns
+		if(ss.getChairs() > 0){
+			ss.occupyChair();
+			store.storeEvent(new Ready(new Time(getTime().getNumTime() + randomTime()), super.customer));
+		}else if(ss.returngetQueue()+ss.getQueue() < ss.getQueueSize()){//det finns plats i kön
+			ss.addToReturnQueue(customer);
+			customer.startQueueTime(getTime());
+		}else if(ss.returngetQueue()+ss.getQueue() >= ss.getQueueSize()){//full kö
+			if(ss.getQueue() > 0){//det finns någon i den vanliga kön
+				ss.rmLastInQueue();
+				ss.addToReturnQueue(customer);
+				customer.startQueueTime(getTime());
+			}else{//return kön är full
+				store.storeEvent(new Return(new Time(getTime().getNumTime() + randomTime()), ss.rmLastInQueue()));
+			}
+		}
 	}
 
 }
